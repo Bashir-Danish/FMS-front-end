e6fae9da689fb939c50aa57bee526fab18de9d5e
Bashir-Danish <bashirdanish124@gmail.com>
2023-12-08 14:21:40 +0430

    issue fixed

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,11 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
 
 const pinia = createPinia();
 const app = createApp(App);
 
-
 app.use(pinia);
-app.component("DatePicker", Vue3PersianDatetimePicker);
 app.use(router);
 
 app.mount("#app");
